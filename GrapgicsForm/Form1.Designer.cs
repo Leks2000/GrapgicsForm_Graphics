5d94b822748ba9544bb53cf191f9495349f77898
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2022-11-10 14:39:14 +0300

    Добавьте файлы проекта.

---
+++ GrapgicsForm/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace GrapgicsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butGraf = new System.Windows.Forms.Button();
+            this.butClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // butGraf
+            // 
+            this.butGraf.BackColor = System.Drawing.Color.Yellow;
+            this.butGraf.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.butGraf.Location = new System.Drawing.Point(12, 419);
+            this.butGraf.Name = "butGraf";
+            this.butGraf.Size = new System.Drawing.Size(171, 50);
+            this.butGraf.TabIndex = 0;
+            this.butGraf.Text = "Наисовать";
+            this.butGraf.UseVisualStyleBackColor = false;
+            this.butGraf.Click += new System.EventHandler(this.butGraf_Click);
+            // 
+            // butClear
+            // 
+            this.butClear.BackColor = System.Drawing.Color.AliceBlue;
+            this.butClear.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.butClear.Location = new System.Drawing.Point(633, 419);
+            this.butClear.Name = "butClear";
+            this.butClear.Size = new System.Drawing.Size(171, 50);
+            this.butClear.TabIndex = 1;
+            this.butClear.Text = "Очистить";
+            this.butClear.UseVisualStyleBackColor = false;
+            this.butClear.Click += new System.EventHandler(this.butClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 481);
+            this.Controls.Add(this.butClear);
+            this.Controls.Add(this.butGraf);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butGraf;
+        private System.Windows.Forms.Button butClear;
+    }
+}
+
---
+++ GrapgicsForm/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrapgicsForm
+{
+  
+    public partial class Form1 : Form
+    {  
+        private Graphics gr;
+        private Pen p;
+        private int  w, h;
+        private double r, s;
+        private int count = 0;
+        LinearGradientBrush gradBrush;
+
+        private void butClear_Click(object sender, EventArgs e)
+        {
+            gr.FillRectangle(new SolidBrush(Color.White), 0, 0, ClientRectangle.Width, ClientRectangle.Height);
+            count++;
+            s = 0;
+        }
+
+        private void butGraf_Click(object sender, EventArgs e)
+        {
+         
+            while (s < 20 * Math.PI )
+            {
+                if (count == 0)
+                {
+
+                    r = s / 2;
+                    var x = r * Math.Cos(s);
+                    var y = r * Math.Sin(s);
+                    var x1 = Math.Round(x * 4) + w;
+                    var y1 = Math.Round(y * 4) + h;
+                    // MessageBox.Show($"{x1}, {y1}");
+                    gr.FillRectangle(gradBrush, (int)x1, (int)y1, 5, 5);
+                    s += 0.01;
+                }
+                else if (count == 1)
+                {
+                    int a = 26, c = 18, d = 14, b = 20;
+                    double k = (a / (double)b) * s;
+                    var x = (a - c) * Math.Cos(s) + d * Math.Cos(k);
+                    var y = (a - c) * Math.Sin(s) - d * Math.Sin(k);
+                    var x1 = Math.Round(x * 10) + w;
+                    var y1 = Math.Round(y * 10) + h;
+                    // MessageBox.Show($"{x1}, {y1}");
+                    gr.FillRectangle(gradBrush, (int)x1, (int)y1, 5, 5);
+                    s += 0.01;
+                }
+                else if (count == 2)
+                {
+                    var x = 6.2 * (Math.Cos(s) - (Math.Cos(3.1 * s) / 3.1));
+                    var y = 6.2 * (Math.Sin(s) - (Math.Sin(3.1 * s) / 3.1));
+                    var x1 = Math.Round(x * 20) + w;
+                    var y1 = Math.Round(y * 20) + h;
+                    // MessageBox.Show($"{x1}, {y1}");
+                    gr.FillRectangle(gradBrush, (int)x1, (int)y1, 5, 5);
+                    s += 0.005;
+                }
+                else if (count == 3)
+                {
+                    var x = 20 * (Math.Cos(s) + (Math.Cos(5 * s) / 5));
+                    var y = 20 * (Math.Sin(s) - (Math.Sin(5 * s) / 5));
+                    var x1 = Math.Round(x * 5) + w;
+                    var y1 = Math.Round(y * 5) + h;
+                    // MessageBox.Show($"{x1}, {y1}");
+                    gr.FillRectangle(gradBrush, (int)x1, (int)y1, 5, 5);
+                    s += 0.005;
+                }
+                else if (count == 4)
+                {
+                    var x = Math.Sin(s) * (Math.Exp(Math.Cos(s)) - 2 * Math.Cos(4 * s) + Math.Pow(Math.Sin((double)1 / 12.0 * s), 5));
+                    var y = -Math.Cos(s) * (Math.Exp(Math.Cos(s)) - 2 * Math.Cos(4 * s) + Math.Pow(Math.Sin((double)1 / 12.0 * s), 5));
+                    var x1 = Math.Round(x * 50) + w;
+                    var y1 = Math.Round(y * 50) + h;
+                    //MessageBox.Show($"{x1}, {y1}");
+                    gr.FillRectangle(gradBrush, (int)x1, (int)y1, 5, 5);
+                    s += 0.001;
+
+                }
+            }
+            butClear_Click(sender, e);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            gr = this.CreateGraphics();
+            var rect = new Rectangle(w, h, this.Width, this.Height);
+            gradBrush = new LinearGradientBrush(rect, Color.Red, Color.Blue,LinearGradientMode.Vertical);
+            s = 0; r = 100; w = this.Width / 2; h = this.Height / 2;
+        }
+    }
+}
